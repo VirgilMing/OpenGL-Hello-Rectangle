e9b6292acb09ec81b89ddd1f42afbe33a35caf1e
Virgil Ming <darth.sidious.m@gmail.com>
2018-05-09 00:24:00 +0800

    split VAO; added colors; added MVP; hardcoded a cube

--- hello-cube/src/draw.cpp
+++ hello-cube/src/draw.cpp
@@ -1,25 +1,152 @@
 #include "include/public.hpp"
 #include "include/draw.hpp"
-
-float points[] = {
-   0.5f,  0.5f,  0.0f,
-   0.5f, -0.5f,  0.0f,
-  -0.5f, -0.5f,  0.0f,
-  -0.5f,  0.5f,  0.0f
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/trigonometric.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+static const GLfloat g_vertex_buffer_data[] = {
+    -1.0f,-1.0f,-1.0f, // triangle 1 : begin
+    -1.0f,-1.0f, 1.0f,
+    -1.0f, 1.0f, 1.0f, // triangle 1 : end
+    1.0f, 1.0f,-1.0f, // triangle 2 : begin
+    -1.0f,-1.0f,-1.0f,
+    -1.0f, 1.0f,-1.0f, // triangle 2 : end
+    1.0f,-1.0f, 1.0f,
+    -1.0f,-1.0f,-1.0f,
+    1.0f,-1.0f,-1.0f,
+    1.0f, 1.0f,-1.0f,
+    1.0f,-1.0f,-1.0f,
+    -1.0f,-1.0f,-1.0f,
+    -1.0f,-1.0f,-1.0f,
+    -1.0f, 1.0f, 1.0f,
+    -1.0f, 1.0f,-1.0f,
+    1.0f,-1.0f, 1.0f,
+    -1.0f,-1.0f, 1.0f,
+    -1.0f,-1.0f,-1.0f,
+    -1.0f, 1.0f, 1.0f,
+    -1.0f,-1.0f, 1.0f,
+    1.0f,-1.0f, 1.0f,
+    1.0f, 1.0f, 1.0f,
+    1.0f,-1.0f,-1.0f,
+    1.0f, 1.0f,-1.0f,
+    1.0f,-1.0f,-1.0f,
+    1.0f, 1.0f, 1.0f,
+    1.0f,-1.0f, 1.0f,
+    1.0f, 1.0f, 1.0f,
+    1.0f, 1.0f,-1.0f,
+    -1.0f, 1.0f,-1.0f,
+    1.0f, 1.0f, 1.0f,
+    -1.0f, 1.0f,-1.0f,
+    -1.0f, 1.0f, 1.0f,
+    1.0f, 1.0f, 1.0f,
+    -1.0f, 1.0f, 1.0f,
+    1.0f,-1.0f, 1.0f
 };
+
+static const GLfloat g_color_buffer_data[] = {
+    0.583f,  0.771f,  0.014f,
+    0.609f,  0.115f,  0.436f,
+    0.327f,  0.483f,  0.844f,
+    0.822f,  0.569f,  0.201f,
+    0.435f,  0.602f,  0.223f,
+    0.310f,  0.747f,  0.185f,
+    0.597f,  0.770f,  0.761f,
+    0.559f,  0.436f,  0.730f,
+    0.359f,  0.583f,  0.152f,
+    0.483f,  0.596f,  0.789f,
+    0.559f,  0.861f,  0.639f,
+    0.195f,  0.548f,  0.859f,
+    0.014f,  0.184f,  0.576f,
+    0.771f,  0.328f,  0.970f,
+    0.406f,  0.615f,  0.116f,
+    0.676f,  0.977f,  0.133f,
+    0.971f,  0.572f,  0.833f,
+    0.140f,  0.616f,  0.489f,
+    0.997f,  0.513f,  0.064f,
+    0.945f,  0.719f,  0.592f,
+    0.543f,  0.021f,  0.978f,
+    0.279f,  0.317f,  0.505f,
+    0.167f,  0.620f,  0.077f,
+    0.347f,  0.857f,  0.137f,
+    0.055f,  0.953f,  0.042f,
+    0.714f,  0.505f,  0.345f,
+    0.783f,  0.290f,  0.734f,
+    0.722f,  0.645f,  0.174f,
+    0.302f,  0.455f,  0.848f,
+    0.225f,  0.587f,  0.040f,
+    0.517f,  0.713f,  0.338f,
+    0.053f,  0.959f,  0.120f,
+    0.393f,  0.621f,  0.362f,
+    0.673f,  0.211f,  0.457f,
+    0.820f,  0.883f,  0.371f,
+    0.982f,  0.099f,  0.879f
+};
+
+
 GLuint vShader, fShader, shaderProgram;
-GLuint vaoHandle, vboHandle;
+GLuint vaoHandle, vboHandle, cboHandle;
 void initVAO(){
-  vboHandle = 0;
-  glGenBuffers(1, &vboHandle);
-  glBindBuffer(GL_ARRAY_BUFFER, vboHandle);
-  glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(float), points, GL_STATIC_DRAW);
-  vaoHandle = 0;
   glGenVertexArrays(1, &vaoHandle);
   glBindVertexArray(vaoHandle);
-  glEnableVertexAttribArray(0);
+}
+void initVBO(){
+  glGenBuffers(1, &vboHandle);
   glBindBuffer(GL_ARRAY_BUFFER, vboHandle);
-  glVertexAttribPointer(0,3, GL_FLOAT, GL_FALSE, 0, NULL);
+  glBufferData(GL_ARRAY_BUFFER, 
+               sizeof(g_vertex_buffer_data), 
+               g_vertex_buffer_data, 
+               GL_STATIC_DRAW);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(
+     0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+     3,                  // size
+     GL_FLOAT,           // type
+     GL_FALSE,           // normalized?
+     0,                  // stride
+     (void*)0            // array buffer offset
+  );
+}
+
+void initCBO(){
+  glGenBuffers(1, &cboHandle);
+  glBindBuffer(GL_ARRAY_BUFFER, cboHandle);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+  // 2nd attribute buffer : colors
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(
+      1,                 // attribute. No particular reason for 1, but must match the layout in the shader.
+      3,                 // size
+      GL_FLOAT,          // type
+      GL_FALSE,          // normalized?
+      0,                 // stride
+      (void*)0           // array buffer offset
+  );
+
+}
+
+
+void createAndSendMVP(){
+  glm::mat4 projectionMat = glm::perspective(
+    glm::radians(45.f),
+    4.0f/3.0f,
+    0.1f,
+    100.0f
+  );
+
+  glm::mat4 viewMat = glm::lookAt(
+    glm::vec3(4,3,3), 
+    glm::vec3(0,0,0),
+    glm::vec3(0,1,0)
+  );
+
+  glm::mat4 modelMat = glm::mat4(1.0f);
+
+  glm::mat4 mvp = projectionMat * viewMat * modelMat;
+
+  GLuint MatrixID = glGetUniformLocation(shaderProgram, "MVP");
+
+  glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
 }
 
 void drawCube(){
@@ -27,5 +154,5 @@ void drawCube(){
   glUseProgram(shaderProgram);
   glBindVertexArray(vaoHandle);
   // draw points 0-4 from the currently bound vao with current in-use shader
-  glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+  glDrawArrays(GL_TRIANGLES, 0, 12*3); // 12*3 indices starting at 0 -> 12 triangles -> 6 squares
 }
