1df30ad0fe0a2efa4342438a04b961a2fbb5e561
Virgil Ming <darth.sidious.m@gmail.com>
2018-05-09 00:26:03 +0800

    reflect mods in draw.cpp

--- hello-cube/src/include/draw.hpp
+++ hello-cube/src/include/draw.hpp
@@ -2,8 +2,10 @@
 #define DRAW_HPP
 void drawCube();
 void initVAO();
+void initVBO();
+void initCBO();
 void initShader(const char *&VShaderFile,
                 const char *&FShaderFile);
-
+void createAndSendMVP();
 extern GLuint vShader, fShader, shaderProgram;
 #endif
